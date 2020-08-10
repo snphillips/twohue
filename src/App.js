e7d48851376faf4edae5e7a1749ceda3063ed5e8
Sarah Phillips <snphillips@gmail.com>
2020-08-10 17:51:47 -0400

    set up initial basic state chart architecture

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Action, withStateMachine, State } from 'react-automata'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const statechart = {
+  initial: 'loading',
+  states: {
+    loading: {
+      on: {
+        READY: 'practice',
+      },
+      onEntry: 'readyAction',
+    },
+    practice: {
+      on: {
+        SELECT_COLOR: 'practice',
+        START_GAME: 'round0',
+      },
+      onEntry: 'sayCiao',
+    },
+    round0: {
+      on: {
+        SELECT_COLOR: 'roundN',
+      },
+      onEntry: 'sayCiao',
+    },
+    attempt0: {
+      on: {
+        SELECT_COLOR: 'roundN',
+      },
+      onEntry: 'sayCiao',
+    },
+    roundN: {
+      on: {
+        UNFINISHED_GAME: 'attemptN',
+        FINISHED_GAME: 'roundsComplete',
+      },
+      onEntry: 'sayCiao',
+    },
+    attemptN: {
+      on: {
+        UNFINISHED_ATTEMPT: 'attemptN',
+        FINISHED_ATTEMPT: 'roundN',
+      },
+      onEntry: 'sayCiao',
+    },
+    roundsComplete: {
+      on: {
+      },
+      onEntry: 'sayCiao',
+    },
+  },
 }
 
-export default App;
+
+
+
+
+
+
+const maxRounds = 10
+const maxAttempts = 6
+
+
+
+class App extends React.Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    round: 0,
+    attempt: 0,
+
+  };
+
+  // This binding is necessary to make `this` work in the callback
+  // this.handleChange = this.handleChange.bind(this);
+
+}
+
+ readyAction = ()=>{this.props.transition('READY')}
+ circle1 = ()=>{this.props.transition('SELECT_COLOR')}
+ circle2 = ()=>{this.props.transition('SELECT_COLOR')}
+
+
+  handleClick = () => {
+    this.props.transition('READY')
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.circle1}>circle 1</button>
+        <button onClick={this.circle2}>circle 2</button>
+        <button onClick={()=>{this.props.transition('START_GAME')}}>START_GAME</button>
+        <State is="loading">SPINNER</State>
+        <State is="practice">practice</State>
+        <State is="round0">game play</State>
+      </div>
+    )
+  }
+}
+
+export default withStateMachine(statechart)(App)
+
